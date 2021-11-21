589f6f2bc8fab4cb043ae85084918603447f8c0b
baloveless <baloveless@icloud.com>
2021-11-21 11:59:45 -0800

    Added a page to choose a genre from. Planning to Populate later with genre facts and a rotating set of show art like home page

--- FA2021AnimeDashboard/wwwroot/js/ChartData.js
+++ FA2021AnimeDashboard/wwwroot/js/ChartData.js
@@ -1,4 +1,57 @@
-﻿let getShowDetails = async (id, category) => {
+﻿// all generes with their array location corresponding to their id
+let genres = [
+	null,
+	'Action',
+	'Adventure',
+	'Cars',
+	'Comedy',
+	'Avante Garde',
+	'Demons',
+	'Mystery',
+	'Drama',
+	null,
+	'Fantasy',
+	'Game',
+	null,
+	'Historical',
+	'Horror',
+	'Kids',
+	null,
+	'Martial Arts',
+	'Mecha',
+	'Music',
+	'Parody',
+	'Samurai',
+	'Romance',
+	'School',
+	'Sci Fi',
+	'Shoujo',
+	null,
+	'Shounen',
+	null,
+	'Space',
+	'Sports',
+	'Super Power',
+	'Vampire',
+	null,
+	null,
+	'Harem',
+	'Slice Of Life',
+	'Supernatural',
+	'Military',
+	'Police',
+	'Psychological',
+	'Suspense',
+	'Seinen',
+	'Josei',
+	null,
+	null,
+	'Award Winning',
+	'Gourmet',
+	'Work Life',
+]
+
+let getShowDetails = async (id, category) => {
 	let Url = 'https://api.jikan.moe/v3/anime/' + id + '/' + category;
 	try {
 		let response = await fetch(Url);
@@ -18,7 +71,7 @@ getShowDetails('1735', 'stats');
 // also contains the rating so you can have bar graphs for how many of each genre 
 // are rated pg-R
 let getShowsByGenre = async (genre) => {
-	let Url = 'https://api.jikan.moe/v3/search/anime?q=&genre='+ genre + '&order_by=score&sort=desc';
+	let Url = 'https://api.jikan.moe/v3/search/anime?q=&genre=' + genre + '&order_by=score&sort=desc';
 	try {
 		let response = await fetch(Url);
 		let data = await response.json();
@@ -28,5 +81,24 @@ let getShowsByGenre = async (genre) => {
     }
 }
 
+function createOptions() {
+	let genreOptions = document.getElementById("showChoice");
+	genreOptions.innerHTML = "";
+	for (i = 0; i < genres.length; i++) {
+		if (genres[i] !== null) {
+			let newOpt = document.createElement("option");
+			newOpt.innerText = genres[i];
+			newOpt.id = '' + i;
+			genreOptions.appendChild(newOpt);
+        }
+	}
+	genreOptions.toggleAttribute("disabled");
+	document.getElementById('search').toggleAttribute('disabled');
+}
+
+createOptions()
+
+
 getShowsByGenre('46');
+
 

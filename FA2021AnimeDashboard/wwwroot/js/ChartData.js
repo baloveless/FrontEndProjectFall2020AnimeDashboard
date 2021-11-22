004289a6fba48f7a6ce938c952e4485613e80a8f
baloveless <baloveless@icloud.com>
2021-11-21 17:07:11 -0800

    Changed chartdata to return the json created

--- FA2021AnimeDashboard/wwwroot/js/ChartData.js
+++ FA2021AnimeDashboard/wwwroot/js/ChartData.js
@@ -56,7 +56,7 @@ let getShowDetails = async (id, category) => {
 	try {
 		let response = await fetch(Url);
 		let data = await response.json();
-		console.log(data);
+		return data;
 	} catch (error) {
 		console.error(error);
     }
@@ -66,16 +66,12 @@ getShowDetails('1735', 'stats');
 
 
 // genre is an id number as a string
-// use this to get a group of shows by genre. 
-// then each one can have their averages taken for a bar graph.
-// also contains the rating so you can have bar graphs for how many of each genre 
-// are rated pg-R
 let getShowsByGenre = async (genre) => {
 	let Url = 'https://api.jikan.moe/v3/search/anime?q=&genre=' + genre + '&order_by=score&sort=desc';
 	try {
 		let response = await fetch(Url);
 		let data = await response.json();
-		console.log(data);
+		return data;
 	} catch (error) {
 		console.error(error);
     }

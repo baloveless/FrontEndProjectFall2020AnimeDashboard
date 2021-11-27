dee45344286d6e339a8d6863234024b691bbdc7d
AlexRheault <alexdrhlt@gmail.com>
2021-11-27 15:09:00 -0800

    Fixing formatting for each of the details page and building out the video page. Details should now be complete!

--- FA2021AnimeDashboard/Pages/Animestuff/Details/VideosDetails.cshtml.cs
+++ FA2021AnimeDashboard/Pages/Animestuff/Details/VideosDetails.cshtml.cs
@@ -1,22 +1,44 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
 namespace FA2021AnimeDashboard.Pages.Animestuff.Details
 {
     public class VideosDetailsModel : PageModel
     {
         public string Anime { get; set; }
+        public videoResponse response { get; set; }
+
         public async Task OnGetAsync(string Anime, int AnimeId)
         {
             this.Anime = Anime;
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://jikan1.p.rapidapi.com/anime/" + AnimeId + "/videos"),
+                Headers =
+    {
+        { "x-rapidapi-host", "jikan1.p.rapidapi.com" },
+        { "x-rapidapi-key", "eacdba1ca3mshbbddb3f551ec180p1cf96djsn47f742ccbd6b" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var myJObject = JsonConvert.DeserializeObject<videoResponse>(body);
+                this.response = (myJObject);
+            }
         }
 
     }
-  
+
     public class promo
     {
         public string title { get; set; }
@@ -25,7 +47,10 @@ namespace FA2021AnimeDashboard.Pages.Animestuff.Details
     }
     public class videoEpisode
     {
-
+        public string title { get; set; }
+        public string episode { get; set; }
+        public string url { get; set; }
+        public string image_url { get; set; }
     }
     public class videoResponse
     {
